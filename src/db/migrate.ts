0cdce3282cb6d360df3dfaf22ceec85021054cea
Morpa <andremorpa@gmail.com>
2024-02-15 06:49:01 +0000

    add env

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,12 +1,16 @@
+import chalk from "chalk";
 import postgres from "postgres";
 
 import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
+import { env } from "../env";
 
-const connection = postgres('postgres://docker:docker@localhost:5432/pizza_shop', { max: 1 })
+const connection = postgres(env.DATABASE_URL, { max: 1 })
 const db = drizzle(connection)
 
 await migrate(db, { migrationsFolder: 'drizzle' })
+
+console.log(chalk.green('Database migrated successfully'))
 
 await connection.end()
 
